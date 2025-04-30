fb4cb1bfcea044c5b556f81aacddd823195cdcd1
PSY <PSY@DESKTOP-DDGUTAE>
2025-04-30 10:50:19 +0900

    3. 변환할 크기(가로, 세로) 및 파일명 받기

--- day250430/src/day250430/Quiz11.java
+++ day250430/src/day250430/Quiz11.java
@@ -1,7 +1,10 @@
 package day250430;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+
+import net.coobird.thumbnailator.Thumbnails;
 
 public class Quiz11 {
 
@@ -17,19 +20,38 @@ public class Quiz11 {
 		String you = scan.nextLine();
 		
 		if(you.equals("")){
-			System.out.println("입력하지 않았어");
+			//System.out.println("입력하지 않았어");
 			test = true;
 			break;
 		}else {
 			fileName = new File(you);
 			if(!fileName.exists()) {
-				System.out.println("없다");
+				//System.out.println("없다");
 				System.out.println("파일이 없습니다. 다시 입력해주세요");
 			}else if(fileName.exists()) {
-				System.out.println("있다");
+				//System.out.println("있다");
 				break;
 			}
 		}
+		}
+		
+		if(fileName.exists()) {
+			System.out.print("변환할 크기의 가로픽셀을 입력해주세요: ");
+			int rkfh = scan.nextInt();
+			System.out.print("변환할 크기의 세로픽셀을 입력해주세요: ");
+			int tpfh = scan.nextInt();
+			scan.nextLine();
+			System.out.print("변환할 파일명을 입력해주세요: ");
+			String youNew = scan.nextLine();
+			File fileNameNew = new File(youNew);
+			try {
+				Thumbnails.of(fileName)
+				.size(rkfh, tpfh)
+				.outputFormat("jpg")
+				.toFile(fileNameNew);
+			}catch (IOException gg) {
+				gg.printStackTrace();
+			}
 		}
 		
 		if(test == true) {
