e4e2dc3ac1937e8f33941b37c3bbb6720049ab09
PSY <PSY@DESKTOP-DDGUTAE>
2025-04-30 10:27:45 +0900

    1-1. 반복문을 완성하지 못하는 중...(1트)

--- day250430/src/day250430/Quiz11.java
+++ day250430/src/day250430/Quiz11.java
@@ -7,14 +7,22 @@ public class Quiz11 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		File fileName = null;
 		
-		//크기를 줄일 파일명 받기
-		System.out.print("크기를 줄일 파일명을 입력해주세요: ");
-		File fileName = new File(scan.nextLine());
-
-		
-		
-		
+		do {
+			//크기를 줄일 파일명 받기
+			System.out.print("크기를 줄일 파일명을 입력해주세요: ");
+			fileName = new File(scan.nextLine());
+				//아무것도 입력하지 않았을 경우, 종료
+				if(!fileName.canRead()) {
+					System.out.println("아무것도 입력 안함");
+				}else if(fileName.exists()){
+					System.out.println("있다");
+					break;
+				}
+		}while(true) { //파일이 없을 경우는 재입력 → 파일이 있을 때까지 반복
+			System.out.println("있다");
+		}
 	}
 
 }
