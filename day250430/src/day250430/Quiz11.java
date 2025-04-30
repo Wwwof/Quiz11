fc417e4d91b5c3315e46c584b07740d61058f556
PSY <PSY@DESKTOP-DDGUTAE>
2025-04-30 10:01:52 +0900

    1. 크기를 줄일 파일명 입력하기

---
+++ day250430/src/day250430/Quiz11.java
@@ -0,0 +1,20 @@
+package day250430;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Quiz11 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		//크기를 줄일 파일명 받기
+		System.out.print("크기를 줄일 파일명을 입력해주세요: ");
+		File fileName = new File(scan.nextLine());
+
+		
+		
+		
+	}
+
+}
